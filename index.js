dbc5282ef1ed5bdf066b510dc5e8b8ebb095a605
Rarri74 <Hammed74@gmail.com>
2023-11-10 11:55:19 -0500

    added more features for mobile

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-const unlikedBtn = document.querySelectorAll(".like-button")
+const unlikedBtn = document.querySelectorAll(".like-button");
 const card = document.querySelectorAll(".projects-panel > a > div");
 
-
-
-unlikedBtn.forEach(heart => {
+unlikedBtn.forEach((heart) => {
   heart.addEventListener("mousedown", function (event) {
     event.stopPropagation();
     let liked = heart.getAttribute("data-liked") === "true";
-      if (liked == true) {
-        heart.classList.remove("animate")
-        heart.src = "assets/love.png";
-      
-    
-      } else {
-        heart.src = "assets/heart.png";
-        heart.classList.add("animate");
-   
-      }
-      heart.setAttribute("data-liked", !liked);
-    });
+    if (liked == true) {
+      heart.classList.remove("animate");
+      heart.src = "assets/love.png";
+    } else {
+      heart.src = "assets/heart.png";
+      heart.classList.add("animate");
+    }
+    heart.setAttribute("data-liked", !liked);
   });
 
-
-card.forEach(cards => {
-    
-
-    cards.addEventListener("mousedown", (event) => {
-        event.preventDefault();
-  cards.style.transform = "translate(10px,10px)";
-      
-  cards.addEventListener("mouseup", () => {
-    cards.style.transform = ""
-  })
-  cards.addEventListener("mouseleave", () => {
-    cards.style.transform = "";
+  heart.addEventListener("touchstart", function (event) {
+    event.stopPropagation();
+    let liked = heart.getAttribute("data-liked") === "true";
+    if (liked == true) {
+      heart.classList.remove("animate");
+      heart.src = "assets/love.png";
+    } else {
+      heart.src = "assets/heart.png";
+      heart.classList.add("animate");
+    }
+    heart.setAttribute("data-liked", !liked);
   });
 });
-})
 
-console.log(window.innerWidth)
+card.forEach((cards) => {
+  cards.addEventListener("touchstart", (event) => {
+    event.preventDefault()
+    cards.style.transform = "translate(10px,10px)";
+  });
+  cards.addEventListener("touchend", (event) => {
+    event.preventDefault();
+    cards.style.transform = "";
+  });
+  cards.addEventListener("mousedown", (event) => {
+    event.preventDefault()
+    cards.style.transform = "translate(10px,10px)";
+
+    cards.addEventListener("mouseup", () => {
+      cards.style.transform = "";
+    });
+    cards.addEventListener("mouseleave", () => {
+      cards.style.transform = "";
+    });
+  });
+});
+
+console.log(window.innerWidth);
